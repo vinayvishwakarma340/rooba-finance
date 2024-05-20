d9f1805afacb50c90c00f4dc7dbdbcd6a518bfa7
Anshu <anshuy115@gmail.com>
2024-05-20 11:35:41 +0530

    routing completed

--- src/components/AnimationFirstPart.js
+++ src/components/AnimationFirstPart.js
@@ -127,12 +127,14 @@ const AnimationFirstPade = () => {
         
         <div className=" container z-50 relative h-full ">
       <div className="bg-black bg-opacity-60 flex sticky  top-[100px] rounded-xl p-2 m-2">
-    <Link target="_self" href={"'/" +"/components/AnimationFirstPart"} className="hover:text-gray-700" ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <Link target="_self" href={"/" } className="hover:text-gray-700" ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
 </svg></Link>  
+<Link target="_self" href={"/" } className="hover:text-gray-700">
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
 </svg>
+</Link>
         </div>  
 
         </div>
@@ -235,12 +237,17 @@ const AnimationFirstPade = () => {
 
         <div className=" container z-10 sticky top-[100px] ">
       <div className="bg-black bg-opacity-60 flex rounded-xl p-2 m-2">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <Link target="_self" href={"/" } className="hover:text-gray-700">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
 </svg>
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    </Link> 
+    <Link target="_self" href={"/" } className="hover:text-gray-700">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
 </svg>
+    </Link>
+
         </div>  
 
         </div>
